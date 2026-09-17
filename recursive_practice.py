681f64e7f67098621b5bcbed68058efd574ea9c2
SHUBHRANIL ROY <shubhranil96@gmail.com>
2026-09-17 21:56:18 +0530

    Implement recursion to print name and number series
    Added a recursive function to print numbers from 0 to target.

--- recursive_practice.py
+++ recursive_practice.py
@@ -2,13 +2,25 @@
 count = 0
 def my_name():
     global count
-    if(count==5):
+    if(count==5): #base function
         return
     else:
         count+=1
         print("hello")
         my_name()
-
 my_name()
+
+
+#recursion to print from 1 - n
+count  = 0
+def num_series(target):
+    global count
+    if(count == target): #base function
+        return
+    else:
+        print(count)
+        count+=1
+        num_series(target)
+num_series(5)
 
 
